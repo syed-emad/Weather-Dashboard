d923a4db920c89d83fdac8aa968c68645047f754
syed-emad <61075900+syed-emad@users.noreply.github.com>
2023-07-07 20:36:38 +0500

    feat: update search state from textbox

--- src/components/cities/Filter.tsx
+++ src/components/cities/Filter.tsx
@@ -2,21 +2,26 @@ import { ChangeEvent } from "react";
 import { TextBox } from "../input/TextBox";
 import { DropDown } from "../input/DropDown";
 import { Heading } from "../titles/Heading";
-
 interface Props {
   search: string;
   setSearch: (search: string) => void;
 }
 export const Filter = ({ search, setSearch }: Props) => {
-  const hanldeTextBoxChange =
-    (key: string) => (evt: ChangeEvent<HTMLInputElement>) => {};
+  const hanldeTextBoxChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setSearch(evt.target.value);
+  };
+
   const handleDropDownChange =
     (key: string) => (evt: ChangeEvent<HTMLSelectElement>) => {};
   return (
     <div className="flex space-x-2 w-full p-6 mb-5 shadow-sm bg-white rouded-md">
       <div className="w-3/4">
         <Heading text="Search" />
-        <TextBox value="" inputKey="search" onChange={hanldeTextBoxChange} />
+        <TextBox
+          value={search}
+          inputKey="search"
+          onChange={hanldeTextBoxChange}
+        />
       </div>
       <div className="w-1/4">
         <Heading text="Country" />
