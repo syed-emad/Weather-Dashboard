6c34c44b6d0ee4d11af8501a0bdfa330e0e16384
syed-emad <61075900+syed-emad@users.noreply.github.com>
2023-07-07 18:55:29 +0500

    feat: add image and color constants

--- src/constants/Colors.tsx
+++ src/constants/Colors.tsx
@@ -1,3 +1,4 @@
 export const COLORS = {
   ThemeBlack: "text-gray-700",
+  Purple: "text-[#81A9FB]",
 };
--- src/constants/Images.tsx
+++ src/constants/Images.tsx
@@ -1,3 +1,6 @@
 export const IMAGES = {
   weather_cloudy: require("../assets/clouds.png"),
+  weather_rain: require("../assets/rain.png"),
+  sunrise: require("../assets/sun.png"),
+  sunset: require("../assets/moon.png"),
 };
